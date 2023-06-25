98712f9fd810106a4407f77b2c138e31a90db19a
Yuliya <julgorina@yandex.ru>
2023-06-26 02:15:00 +0300

    Add project files.

---
+++ CHAT_WCF_GY1/IServiceChat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CHAT_WCF_GY1
+{
+    [ServiceContract(CallbackContract = typeof(IServerChatCallback))]
+    public interface IServiceChat
+    {
+        [OperationContract]
+        int Connect(string name);
+
+        [OperationContract]
+        void Disconnect(int id);
+
+        [OperationContract(IsOneWay = true)]
+        void SendMessage(string msg, int id);
+    }
+
+    public interface IServerChatCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void MessageCallback(string msg);
+    }
+}
